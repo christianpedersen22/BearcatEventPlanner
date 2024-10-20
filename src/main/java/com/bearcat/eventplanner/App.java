5ecdaeefcf433ab07241a6884425abfc872cbf4a
Christian Pedersen <126288116+christianpedersen22@users.noreply.github.com>
2024-10-20 16:59:47 -0400

    Feature/authsetup (#2)
    * Auth Class Setup
    
    * Readding the effective-pom.xml

--- src/main/java/com/bearcat/eventplanner/App.java
+++ src/main/java/com/bearcat/eventplanner/App.java
@@ -1,7 +1,12 @@
 package com.bearcat.eventplanner;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
         System.out.println("Hello, Bearcat Event Planner!");
     }
 }
