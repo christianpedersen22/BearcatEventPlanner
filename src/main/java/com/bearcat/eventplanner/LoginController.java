15ec91d8780f698fb57ff63ac4fac3e198d2d19e
Danny Murray <murraydp@mail.uc.edu>
2024-11-03 20:11:22 -0500

    Added javadoc describing the application's controllers

--- src/main/java/com/bearcat/eventplanner/LoginController.java
+++ src/main/java/com/bearcat/eventplanner/LoginController.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller class for the login page
+ */
 @Controller
 public class LoginController {
+    /**
+     * Front-end endpoint used to access the application's login page
+     * @return An instance of the "login" thymeleaf template
+     */
     @RequestMapping("/login")
     public String login() {
         return "login";
--- src/main/java/com/bearcat/eventplanner/UserProfileController.java
+++ src/main/java/com/bearcat/eventplanner/UserProfileController.java
@@ -7,14 +7,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller class for the user profile page
+ */
 @Controller
 public class UserProfileController {
 
+    /**
+     * Front-end endpoint used to access the root page of the application
+     * @return String redirecting the user to the "/profile" endpoint
+     */
     @RequestMapping("/")
     public String home() {
         return "redirect:/profile";  // Redirect root URL to /profile
     }
 
+    /**
+     * Front-end endpoint used to access the application's profile page
+     * @param model Model used to pass data to the "profile" thymeleaf template
+     * @return An instance of the "profile" thymeleaf template
+     */
     @RequestMapping("/profile")
     public String profile(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
