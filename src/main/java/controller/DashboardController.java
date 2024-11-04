514977f8412c3f46322b902ef8ee6d2eb3fcb82e
David Rome <romedt@mail.uc.edu>
2024-11-03 21:31:11 -0500

    add methods for creating and listing events

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import com.bearcat.eventplanner.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DashboardController {
@@ -10,5 +16,19 @@ public class DashboardController {
     public String getDashboard(Model model) {
         model.addAttribute("events", "Today's Events");
                 return "dashboard"; //dashboard.html
+    }
+    private List<Event> events = new ArrayList<>();
+
+    //create a new event
+    @PostMapping("/create")
+    public Event createEvent(@RequestBody Event event){
+        events.add(event);
+        return event; //return newly created event
+    }
+
+    //get list of all events
+    @GetMapping("/events")
+    public List<Event> getEvents(){
+        return events;
     }
 }
